664bc477f990f9a7fbd6f5f8f9f46084fc9f0ffb
AMIT SHUKLA <amit800blp@gmail.com>
2024-05-23 18:34:15 +0530

    delete some files

--- project3/accounts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import login_view, register_view, logout_view
-urlpatterns = [
-    path('login/', login_view, name='login'),
-    path('register/', register_view, name='register'),
-    path('logout/', logout_view, name='logout')
-]
